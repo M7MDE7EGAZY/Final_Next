59cba0139ec6acbcab5f175c25e4f9bb0cbe8465
M7MDE7EGAZY <mo7.work@outlook.com>
2024-10-09 19:24:30 +0300

    re-add e2e fixed
    Signed-off-by: M7MDE7EGAZY <mo7.work@outlook.com>

--- src/app/product-page/product-page-routing.module.ts
+++ src/app/product-page/product-page-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from '../app.component'; // Your main page component
 import { ProductPageComponent } from '../product-page/product-page.component'; // Your product page component
 
 const routes: Routes = [
-  { path: '', component: AppComponent }, // Default route for index.html
   { path: 'product-page', component: ProductPageComponent }, // Route for product page
 ];
 
